0d6629fd645c3038a909df7357efe03867fab69a
zhoelzhelev <zhoel_zhelev2004@abv.bg>
2025-08-04 21:58:40 +0300

    Add files via upload

---
+++ Scedulefy.OCommon/ValidationConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace Schedulefy.OCommon
+{
+    public static class ValidationConstants
+    {
+        public static class Event
+        {
+            public const int EventNameMinLength = 5;
+            public const int EventNameMaxLength = 50;
+
+            public const int EventDescriptionMinLength = 10;
+            public const int EventDescriptionMaxLength = 500;
+
+            public const int PublishedOnlength = 10;
+            public const string PublishedOnCorrectFormat = "dd.MM.yyyy";
+        }
+
+        public static class Category
+        {
+            public const int CategoryNameMinLength = 3;
+            public const int CategoryNameMaxLength = 20;
+        }
+    }
+}
