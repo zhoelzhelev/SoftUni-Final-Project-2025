7f8614c926be1b5b7821ec420f8ee9cbc40c14b8
zhoelzhelev <zhoel_zhelev2004@abv.bg>
2025-08-04 22:04:14 +0300

    Add files via upload

---
+++ Schedulefy.ViewModels/Events/DetailsEventViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedulefy.ViewModels.Events
+{
+    public class DetailsEventViewModel : BaseEventViewModel
+    {
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ Schedulefy.ViewModels/Tickets/BuyTicketViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedulefy.ViewModels.Tickets
+{
+    public class BuyTicketViewModel
+    {
+        public int EventId { get; set; }
+        public string EventTitle { get; set; } = null!;
+        public string? EventImageUrl { get; set; }
+        public decimal PricePerTicket { get; set; }
+    }
+}
