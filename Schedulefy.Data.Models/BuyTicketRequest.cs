0253159b95f8df06e89e1692950e3f871f1280c6
zhoelzhelev <zhoel_zhelev2004@abv.bg>
2025-08-04 22:01:20 +0300

    Add files via upload

---
+++ Schedulefy.Data.Models/BuyTicketRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedulefy.Data.Models
+{
+    public class BuyTicketRequest
+    {
+        public int EventId { get; set; }
+        public int Quantity { get; set; }
+        public decimal  Price { get; set; }
+    }
+}
---
+++ Schedulefy.Data.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Schedulefy.OCommon.ValidationConstants.Category;
+
+namespace Schedulefy.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(CategoryNameMaxLength, MinimumLength = CategoryNameMinLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual IEnumerable<Event> Events { get; set; } = 
+            new List<Event>();
+    }
+}
