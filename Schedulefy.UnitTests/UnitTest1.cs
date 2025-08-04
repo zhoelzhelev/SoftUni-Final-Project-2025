df7949a210dbeaa2824b098a40ed406fd34fa65c
zhoelzhelev <zhoel_zhelev2004@abv.bg>
2025-08-04 22:03:30 +0300

    Add files via upload

---
+++ Schedulefy.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Schedulefy.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
