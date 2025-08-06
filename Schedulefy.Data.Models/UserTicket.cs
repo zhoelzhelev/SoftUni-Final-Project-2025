3d76bda5ee0a243ab1e37d3fc595577d3942541f
zhoelzhelev <zhoel_zhelev2004@abv.bg>
2025-08-06 19:20:59 +0300

    Add files via upload

---
+++ Schedulefy.Data.Models/UserTicket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Schedulefy.Data.Models
+{
+    public class UserTicket
+    {
+        [Key]
+        public int UserTicketId { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; } = null!;
+
+        public virtual IdentityUser User { get; set; } = null!;
+
+        [ForeignKey(nameof(Ticket))]
+        public int TicketId { get; set; }
+
+        public virtual Ticket Ticket { get; set; } = null!;
+
+        public int TickeetsCount { get; set; }
+    }
+}
