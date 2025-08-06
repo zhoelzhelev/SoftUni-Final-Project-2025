f1938a5a3c96dddc721c0a2d64dd33a7946900f9
zhoelzhelev <zhoel_zhelev2004@abv.bg>
2025-08-06 19:21:58 +0300

    Add files via upload

---
+++ Schedulefy.Services.Core/CategoryService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Schedulefy.Data;
+using Schedulefy.Services.Core.Contracts;
+using Schedulefy.ViewModels.Events;
+
+namespace Schedulefy.Services.Core
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly SchedulefyDbContext _context;
+
+        public CategoryService(SchedulefyDbContext context)
+        {
+            this._context = context;
+        }
+
+        
+
+        public async Task<IEnumerable<AddCategoriesDropDownMenu>> GetAllCategoriesAsync()
+        {
+            IEnumerable<AddCategoriesDropDownMenu> dropDownMenu = await this._context
+                .Categories
+                .Select(c => new AddCategoriesDropDownMenu
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                })
+                .ToArrayAsync();
+
+            return dropDownMenu;
+        }
+    }
+}
---
+++ Schedulefy.Services.Core/Contracts/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Schedulefy.ViewModels.Events;
+
+namespace Schedulefy.Services.Core.Contracts
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<AddCategoriesDropDownMenu>> GetAllCategoriesAsync();
+    }
+}
