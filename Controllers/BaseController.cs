86331c5455461caf148f43e472b0a6cbc3e6a650
zhoelzhelev <zhoel_zhelev2004@abv.bg>
2025-08-12 17:46:55 +0300

    Add files via upload

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Schedulefy.Controllers;
+
+[Authorize]
+public abstract class BaseController : Controller
+{
+    protected bool IsUerAuthenticated()
+    {
+        return this.User.Identity?.IsAuthenticated ?? false;
+    }
+
+    protected string? GetUserId()
+    {
+        string? userId = null;
+
+        bool isAuthenticated = this.IsUerAuthenticated();
+
+        if (isAuthenticated)
+        {
+            userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        return userId;
+    }
+}
---
+++ Controllers/CommentController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Schedulefy.Services.Core;
+using Schedulefy.Services.Core.Contracts;
+using Schedulefy.ViewModels.Comments;
+
+namespace Schedulefy.Controllers
+{
+    public class CommentController : BaseController
+    {
+        private readonly ICommentService _commentService;
+
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int id)
+        {
+            try
+            {
+                IEnumerable<ComentIndexViewModel> model = await this._commentService
+                    .LoadAllCommentsForCurrentEvent(id);
+
+                ViewData["EventId"] = id;
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return this.RedirectToAction("Details", "Event", id);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Add(int id)
+        {
+            try
+            {
+                AddCommentInputModel model = new AddCommentInputModel() 
+                {
+                    EventId = id
+                };
+
+                return this.View(model);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return this.RedirectToAction("Index", "Event");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddCommentInputModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return this.View(model);
+                }
+
+                string? userId = this.GetUserId();
+
+                if (userId is null) 
+                {
+                    return this.View(model);
+                }
+
+                bool result = await this._commentService.AddCommentAsync(model, userId);
+
+                if (!result) 
+                {
+                    return this.View(model);
+                }
+
+                return this.RedirectToAction("Details","Event", model.EventId);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+                return this.RedirectToAction("Index", "Event");
+            }
+        }
+    }
+}
